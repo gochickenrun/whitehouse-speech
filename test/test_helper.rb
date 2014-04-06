7892bba3c0ce927bda736ab545b513945bed2b70
Sherman Mui <sherman@meatchunks.com>
2014-04-06 15:45:44 -0700

    Remove the Rack mentions

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,16 +1,7 @@
 require 'minitest/autorun'
-require 'rack/test'
 
 require 'whitehouse_speech'
 
 def file_fixture(name)
   File.expand_path "../fixtures/#{name}", __FILE__
 end
-
-# class MiniTest::Unit::TestCase
-#   include Rack::Test::Methods
-# end
-
-# class MiniTest::Unit::Spec
-#   include Rack::Test::Methods
-# end
